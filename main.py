f4424b6e4efbdba14edeff9aff98a75d756b0b2d
MeoTabeo <torsten@willitzkat.de>
2025-06-29 11:48:54 +0200

    CH3, L2: arguments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, count_characters, sort_dict
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,8 +7,14 @@ def get_book_text(filepath):
     
 
 def main():
-    booktext = get_book_text("./books/frankenstein.txt")
-    
+    #booktext = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path_to_book = sys.argv[1]
+    booktext = get_book_text(path_to_book)
+
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
     print("----------- Word Count ----------")
@@ -16,12 +23,14 @@ def main():
     print("--------- Character Count -------")
     number_of_chars = count_characters(booktext)
     dict_list = sort_dict(number_of_chars)
-    
+       
     for dict in dict_list:
         if dict["char"].isalpha():
             char = dict["char"]
             number = dict["num"] 
             print(f"{char}: {number}")
+    
+    print("============= END ===============")
 
     
 
