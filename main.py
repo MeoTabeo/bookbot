8dbe35c9467410d07c67fc63c37bcdbefa95e292
MeoTabeo <torsten@willitzkat.de>
2025-06-29 10:48:24 +0200

    count words

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
+    
+def count_words(text):
+    word_list = text.split()
+    count = len(word_list)
+    return count
+
 
 def main():
     booktext = get_book_text("./books/frankenstein.txt")
-    print(booktext)
+    # print(booktext)
+    number_of_words = count_words(booktext)
+    print(f"{number_of_words} words found in the document")
 
 main()
